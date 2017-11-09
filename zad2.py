afbfeed904ef2ba79e240dbe9fe5ec82a1cd8b37
Kanzey <kamil1pl@gmail.com>
2017-11-09 20:33:22 +0100

    Added skale for gray grading

--- zad2.py
+++ zad2.py
@@ -41,12 +41,15 @@ class PixelImage(Canvas):
 ##                cords, fill="white", outline="white")
 ##            self.pixelValues[x][y] = 0
                 
-    def loadImage(self):
-        file_path = tkFileDialog.askopenfilename()
-        img = Image.open(file_path)
-        tmp = ImageOps.fit(img,(self.xCellNum, self.yCellNum), Image.ANTIALIAS)
-        gray = tmp.convert('L')
-        bw = gray.point(lambda x: 0 if x<128 else 255, '1').load()
+    def loadImage(self, step, doReload=None):
+        if doReload == None:
+            doReload = False
+        if doReload == False:
+            file_path = tkFileDialog.askopenfilename()
+            img = Image.open(file_path)
+            tmp = ImageOps.fit(img,(self.xCellNum, self.yCellNum), Image.ANTIALIAS)
+            self.gray = tmp.convert('L')
+        bw = self.gray.point(lambda x: 0 if x<step else 255, '1').load()
         start = time.time()
         for x in xrange(self.xCellNum):
             for y in xrange(self.yCellNum):
@@ -107,6 +110,16 @@ class PixelImage(Canvas):
             else:
                 self.itemconfig(self.pixelRects[x][y], fill="white", outline="white")
                 self.pixelValues[x][y] = 0;
+    def onclick(self,event):
+        self.reversePixel(event.x/10,event.y/10)
+
+    def reversePixel(self, x,y):
+        if self.pixelValues[x][y] == 0:
+            self.itemconfig(self.pixelRects[x][y], fill="black", outline="gray")
+            self.pixelValues[x][y] = 1;
+        else:
+            self.itemconfig(self.pixelRects[x][y], fill="white", outline="white")
+            self.pixelValues[x][y] = 0;
         
 
 class GUI:
@@ -117,6 +130,7 @@ class GUI:
         frame = Frame(master, width=fWidth, height=fHeight)
         frame.pack(side="left")
         self.inCanvas = PixelImage(frame, width=fWidth, height=fHeight)
+        self.inCanvas.bind("<Button-1>", self.inCanvas.onclick)
         self.inCanvas.pack()
         self.inCanvas.createPixels(imgWidth, imgHeight, 10)
         
@@ -129,14 +143,32 @@ class GUI:
         
         btn = Button(master, text="load", command= lambda: self.loadImage())
         btn.pack()
-        btn = Button(master, text="Add noise", command= lambda: self.inCanvas.changePixels(300))
+        e= Entry(master)
+        e.pack()
+        e.insert(0,'300')
+        btn = Button(master, text="Add noise", command= lambda: self.inCanvas.changePixels(int(e.get())))
         btn.pack()
-
-        #def createButtonDisplay(self, parent, width, height):
+        self.w = Scale(master, from_=0, to=255, orient=HORIZONTAL)
+        self.w.set(128)
+        btn = Button(master, text="reload", command= self.reloadImage)
+        btn.pack(side="bottom")
+        self.w.pack(side="bottom")
+        self.isBusy = False
+        self.wasImageLoaded = False
 
         
+
+        #def createButtonDisplay(self, parent, width, height):
+    def reloadImage(self):
+        if self.isBusy == False and self.wasImageLoaded:
+            self.isBusy = True
+            self.inCanvas.loadImage(self.w.get(), doReload = True)
+            self.isBusy = False
+        
     def loadImage(self):
-        self.inCanvas.loadImage()
+        self.wasImageLoaded = True
+        self.inCanvas.loadImage(self.w.get())
+
 
 
 root = Tk()
