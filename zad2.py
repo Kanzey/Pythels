3a121cc6600fda4ff5ff4e78f3a554d56917d080
Kanzey <kamil1pl@gmail.com>
2017-11-09 10:52:51 +0100

    Added loadImage function

--- zad2.py
+++ zad2.py
@@ -2,28 +2,33 @@ from Tkinter import *
 from PIL import Image, ImageOps, ImageTk
 
 class GUI:
-    def __init__(self, master):
-        self.master = master
-        basewidth = 50
-        img = Image.open('test2.jpg')
-        tmp = ImageOps.fit(img,(basewidth,basewidth), Image.ANTIALIAS)
+    def __init__(self, master, imgWidth, imgHeight):
+        self.master, self.imgWidth, self.imgHeight = master, imgWidth, imgHeight
+        fWidth, fHeight = 10* imgWidth, 10* imgHeight
+        frame = Frame(master, width=fWidth, height=fHeight)
+        frame.pack()
+        self.btn=[[0 for x in xrange(imgHeight)] for x in xrange(imgWidth)] 
+        for x in range(self.imgWidth):
+            for y in range(self.imgHeight):
+                f = Frame(frame, width=10, height=10)
+                f.grid(column=x, row=y)
+                f.pack_propagate(0)
+                self.btn[x][y] = Button(f, bg="white")
+                self.btn[x][y].pack()
+        self.loadImage(self.btn, "test2.jpg")
+                
+    def loadImage(self, target, imageName):   
+        img = Image.open(imageName)
+        tmp = ImageOps.fit(img,(self.imgWidth, self.imgHeight), Image.ANTIALIAS)
         gray = tmp.convert('L')
         bw = gray.point(lambda x: 0 if x<128 else 255, '1')
-        frame = Frame(master, width=500, height=500)
-        frame.pack()
-        for i in range(50):
-            for k in range(50):
-                f = Frame(frame, width=10, height=10)
-                f.grid(column=i, row=k)
-                f.pack_propagate(0)
-                if bw.getpixel((i,k)) == 0:
-                    btn = Button(f, bg="black")
+        for x in xrange(self.imgWidth):
+            for y in xrange(self.imgHeight):
+                if bw.getpixel((x,y)) == 0:
+                    target[x][y].config(bg="black")
                 else:
-                    btn = Button(f, bg="white")
-                btn.pack()
-
-                
+                    target[x][y].config(bg="white")
 
 root = Tk()
-my_gui = GUI(root)
+my_gui = GUI(root, 50, 50)
 root.mainloop()
